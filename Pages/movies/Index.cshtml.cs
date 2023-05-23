187ae0340eccc50e75703a7c98509bb301184d9d
RakotosonFabien <rakotosonfabienmaminirina@gmail.com>
2023-05-23 14:50:54 +0300

    Test filtre

--- Pages/movies/Index.cshtml.cs
+++ Pages/movies/Index.cshtml.cs
@@ -7,13 +7,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CourrierDocker_MBDS_31.Data;
 using CourrierDocker_MBDS_31.modeles;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CourrierDocker_MBDS_31.Pages.movies
 {
     public class IndexModel : PageModel
     {
         private readonly CourrierDocker_MBDS_31.Data.CourrierDocker_MBDS_31Context _context;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
 
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
         public IndexModel(CourrierDocker_MBDS_31.Data.CourrierDocker_MBDS_31Context context)
         {
             _context = context;
@@ -23,10 +30,14 @@ namespace CourrierDocker_MBDS_31.Pages.movies
 
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Movie = await _context.Movie.ToListAsync();
+                movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
